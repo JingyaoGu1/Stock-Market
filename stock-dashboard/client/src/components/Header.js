4991157b68f36dd80496904e9e5b9ba42e6166bb
Jingyao Gu <43628019+JingyaoGu1@users.noreply.github.com>
2023-11-24 11:35:29 -0800

    predict functionalities complete

--- stock-dashboard/client/src/components/Header.js
+++ stock-dashboard/client/src/components/Header.js
@@ -1,54 +1,16 @@
-// import React from "react";
-// import Search from "./Search";
-// import ThemeIcon from "./ThemeIcon";
-
-// const Header = ({ name }) => {
-//   return (
-//     <>
-//       <div className="xl:px-32">
-//         <h1 className="text-5xl">{name}</h1>
-//         <Search />
-//       </div>
-//       <ThemeIcon />
-//     </>
-//   );
-// };
-
-// export default Header;
-
-import React, { useState, useContext } from "react";
+import React from "react";
 import Search from "./Search";
 import ThemeIcon from "./ThemeIcon";
-import StockContext from "../context/StockContext";
+import Predict from "./Predict";
 
 const Header = ({ name }) => {
-  const [predictedValue, setPredictedValue] = useState('');
-
-  const { stockSymbol } = useContext(StockContext);
-
-  const handlePredictClick = async () => {
-    try {
-      console.log("Predict button clicked");
-      const response = await fetch(`http://127.0.0.1:5000/predict/${stockSymbol}`);
-      const data = await response.json();
-      console.log("API response:", data);
-      setPredictedValue(data.predictedPrice); // Ensure this matches the API response
-      console.log("Predicted Value state updated to:", data.predictedPrice);
-    } catch (error) {
-      console.error('Error fetching prediction:', error);
-    }
-  };
-
   return (
     <>
-      <div className="xl:px-32 flex justify-between items-center">
-        <div>
-          <h1 className="text-5xl">{name}</h1>
+      <div className="xl:px-32">
+        <h1 className="text-5xl">{name}</h1>
+        <div className="flex flex-row justify-between items-center space-x-4">
           <Search />
-        </div>
-        <div>
-          <button onClick={handlePredictClick}>Predict</button>
-          {predictedValue && <p>Predicted Value: {predictedValue}</p>}
+          <Predict />
         </div>
       </div>
       <ThemeIcon />
@@ -57,3 +19,45 @@ const Header = ({ name }) => {
 };
 
 export default Header;
+
+// import React, { useState, useContext } from "react";
+// import Search from "./Search";
+// import ThemeIcon from "./ThemeIcon";
+// import StockContext from "../context/StockContext";
+
+// const Header = ({ name }) => {
+//   const [predictedValue, setPredictedValue] = useState('');
+
+//   const { stockSymbol } = useContext(StockContext);
+
+//   const handlePredictClick = async () => {
+//     try {
+//       console.log("Predict button clicked");
+//       const response = await fetch(`http://127.0.0.1:5000/predict/${stockSymbol}`);
+//       const data = await response.json();
+//       console.log("API response:", data);
+//       setPredictedValue(data.predictedPrice); // Ensure this matches the API response
+//       console.log("Predicted Value state updated to:", data.predictedPrice);
+//     } catch (error) {
+//       console.error('Error fetching prediction:', error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="xl:px-32 flex justify-between items-center">
+//         <div>
+//           <h1 className="text-5xl">{name}</h1>
+//           <Search />
+//         </div>
+//         <div>
+//           <button onClick={handlePredictClick}>Predict</button>
+//           {predictedValue && <p>Predicted Value: {predictedValue}</p>}
+//         </div>
+//       </div>
+//       <ThemeIcon />
+//     </>
+//   );
+// };
+
+// export default Header;
